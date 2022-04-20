6a464e479516ef9218401df5c64a5fe12140d4da
Facundo Abbas <facundoabbas@hotmail.com>
2022-04-20 18:58:29 -0300

    Commit antes del brench}

---
+++ Punto1.c
@@ -0,0 +1,125 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+struct Tarea {
+    int TareaID; //Numerado en ciclo iterativo
+    char *Descripcion; //
+    int Duracion; // entre 10 – 100
+}typedef Tarea;
+
+
+
+int main(){
+    srand(time(NULL));
+    Tarea **Tareas, **TareasListas, **TareasNListas ;
+    int cantidadT;
+    int j=0;
+    int z=0;
+    printf("Cuantas tarea desea ingresar?\n");
+    scanf("%i",&cantidadT);
+    Tareas = (Tarea **)malloc(cantidadT * sizeof(Tarea *));
+    for (int i = 0; i < cantidadT; i++)
+    {
+        Tareas[i]  = (Tarea *)malloc(sizeof(Tarea)); 
+        Tareas[i]->TareaID = i+1;
+        Tareas[i]->Descripcion = (char *)malloc(500);
+        printf("Haga la descripcion a la tarea \n");
+        fflush(stdin);
+        gets(Tareas[i]->Descripcion);
+        fflush(stdin);
+        Tareas[i]->Duracion = rand()%91 + 10;
+    }
+    TareasListas = (Tarea **)malloc(cantidadT * sizeof(Tarea *));
+    TareasNListas = (Tarea **)malloc(cantidadT * sizeof(Tarea *));
+
+    for (int i = 0; i < cantidadT; i++)
+    {
+        
+        TareasListas[i] = NULL;
+    }
+    for (int i = 0; i < cantidadT; i++)
+    {
+        
+        TareasNListas[i] = NULL;
+    }
+    printf("Lista de Tareas \n");
+    for (int i = 0; i < cantidadT; i++)
+    {
+        printf("El id de la tarea es : %d \n", Tareas[i]->TareaID);
+        fflush(stdin);
+        printf("La descripcion de la tarea es: \n");
+        fflush(stdin);
+        puts(Tareas[i]->Descripcion);
+        fflush(stdin);
+        printf("La duracion es de %d \n", Tareas[i]->Duracion);
+        char opc;
+        
+        printf("La tarea esta completada? Si esta completada ingrese: Y(si) o N(No)\n");
+        scanf("%c",&opc);
+        if (opc == 'Y')
+        {
+            TareasListas[j] = Tareas[i];
+            Tareas[i] = NULL;
+            j++;
+        }else
+        {
+            TareasNListas[z] = Tareas[i];
+            Tareas[i] = NULL;
+            z++;
+        }
+        
+        
+
+    }
+    printf("Mostraremos cada una de las tareas realizadas \n");
+    for (int i = 0; i < j; i++)
+    {
+        if (TareasListas[i] == NULL)
+        {
+            /* code */
+        }else
+        {
+            printf("El id de la tarea es : %d \n", TareasListas[i]->TareaID);
+            printf("La descripcion de la tarea es: \n");
+       
+            puts(TareasListas[i]->Descripcion);
+       
+            printf("La duracion es de %d \n", TareasListas[i]->Duracion);
+        }
+        
+        
+       
+    }
+    printf("Mostraremos cada una de las tareas NO realizadas \n");
+    for (int i = 0; i < z; i++)
+    {
+        if (TareasNListas[i] == NULL)
+        {
+            /* code */
+        }else
+        {
+            printf("El id de la tarea es : %d \n", TareasNListas[i]->TareaID);
+            printf("La descripcion de la tarea es: \n");
+            
+            puts(TareasNListas[i]->Descripcion);
+        
+            printf("La duracion es de %d \n", TareasNListas[i]->Duracion);
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+
+
+
+
+
+
+    return 0;
+}
